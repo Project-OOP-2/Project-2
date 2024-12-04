3bc434aab2a715af606a2da9455ea8615128de93
Malena <malenasantos774@gmail.com>
2024-12-04 10:28:42 -0300

    Update setup.py
    finalização do setup

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="finance_backend", 
+    version="0.1.0",  
+    description="Backend de um app de finanças",
+    long_description=open("README.md").read(),
+    license="MIT", 
+    author="Malena Milani dos Santos, Wangleycia Lorena Silva de Oliveira",
+    python_requires=">=3.8", 
+    install_requires=[], 
+    url="https://github.com/Project-OOP-2/Project-2.git",
+    packages=find_packages(), 
+
+)
